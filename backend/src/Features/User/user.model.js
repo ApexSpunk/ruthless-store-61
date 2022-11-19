c9b36e494948bf08ba36fe2f44b227f6651b9cc9
AkashPanda1701 <akashpanda1701@gmail.com>
2022-11-19 12:27:05 +0530

    back

---
+++ backend/src/Features/User/user.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    phone:{
+        type: Number,
+        required: true
+
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+}, {
+    versionKey: false,
+}
+);
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
